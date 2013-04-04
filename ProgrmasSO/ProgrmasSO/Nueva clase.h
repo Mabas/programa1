5928a86bae25a1abfe074df4894281990874a2cd
Luis Alberto Ortiz <MrCalamitus@MacbookProMrC.local>
2013-04-04 11:39:06 -0600

    Se modifico la Clase nueva

---
+++ ProgrmasSO/ProgrmasSO/Nueva clase.h
@@ -0,0 +1,13 @@
+//
+//  Nueva clase.h
+//  ProgrmasSO
+//
+//  Created by Luis Alberto Ortiz on 04/04/13.
+//  Copyright (c) 2013 Luis Alberto Ortiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Nueva_clase : NSObject
+
+@end
---
+++ ProgrmasSO/ProgrmasSO/Nueva clase.m
@@ -0,0 +1,13 @@
+//
+//  Nueva clase.m
+//  ProgrmasSO
+//
+//  Created by Luis Alberto Ortiz on 04/04/13.
+//  Copyright (c) 2013 Luis Alberto Ortiz. All rights reserved.
+//
+
+#import "Nueva clase.h"
+
+@implementation Nueva_clase
+
+@end
